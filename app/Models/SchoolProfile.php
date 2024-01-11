ba9c0e098f5c867d8fa9e81b26e1cbf0716d0310
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-11 13:47:14 +0300

    School profile model

---
+++ app/Models/SchoolProfile.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SchoolProfile extends Model
+{
+    use HasFactory;
+
+    protected $guarded =[];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function county(): BelongsTo {
+        return $this->belongsTo(County::class);
+    }
+
+    public function constituency(): BelongsTo {
+        return $this->belongsTo(Constituency::class);
+    }
+
+    public function ward(): BelongsTo {
+        return $this->belongsTo(Ward::class);
+    }
+}
